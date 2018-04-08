4e6a0394cb7b332b5f6c25c9f0d63339497428f9
Sai <svemprala@asu.edu>
2018-04-07 20:30:01 -0500

    Modifications

--- src/test.cpp
+++ src/test.cpp
@@ -2,6 +2,8 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/io/ply_io.h>
+#include <pcl/console/parse.h>
+#include <pcl/common/transforms.h>
 #include <pcl_conversions/pcl_conversions.h>
 // define the following in order to eliminate the deprecated headers warning
 #define VTK_EXCLUDE_STRSTREAM_HEADERS
@@ -20,21 +22,26 @@ int main(int argc, char** argv)
 	ros::Publisher pub;
 	ros::Rate loop_rate(10);
 	ROS_INFO("Attempting to read PLY file");
-    PointCloud<PointXYZRGBNormal> pointCloud;
-    if (loadPLYFile("/home/sai/Dropbox/ICUAS/ICUAS tests/urbanSmall2/reconstruction_sequential/cloud_and_poses.ply", pointCloud) != 0) {
+    PointCloud<PointXYZRGB> pointCloud, pointCloudScaled;
+    if (loadPLYFile("/home/sai/Dropbox/AHS/Apr5/reconstruction_sequential/cloud_and_poses2.ply", pointCloud) != 0) {
 		ROS_ERROR("Unable to read ply file");
 		return 1;
 	}
+
+	Eigen::Matrix4f transform = Eigen::Matrix4f::Identity(); 
+	transform(2,3) = 10;
+	pcl::transformPointCloud (pointCloud, pointCloudScaled, transform); 
+
     pub = n.advertise<sensor_msgs::PointCloud2> ("output", 1);
 
 	while(ros::ok())
     {
 		sensor_msgs::PointCloud2 pointCloudMessage;
     	// Define PointCloud2 message.
-    	toROSMsg(pointCloud, pointCloudMessage);
+    	toROSMsg(pointCloudScaled, pointCloudMessage);
 		
 		pointCloudMessage.header.stamp = ros::Time::now();
-		pointCloudMessage.header.frame_id = "world";
+		pointCloudMessage.header.frame_id = "camera";
 		pub.publish (pointCloudMessage);
 	    ros::spinOnce();
 		loop_rate.sleep();
